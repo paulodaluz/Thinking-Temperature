f8ed52afcfcd08dc60a14261112fda3368314b42
Paulo da Luz <paulo.luz@compasso.com.br>
2019-10-18 17:20:00 -0300

    Criando Service

---
+++ frontend/src/services/apiGets.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+
+let api = "";
+
+export const automaticModeAirConditioning = () => {
+    return new Promise((resolve, reject) => {
+        axios.get(`${api}/people/`)
+            .then(dados => {
+                // setTimeout(function () { resolve(dados.data.results) }, 99999000)
+                resolve(dados.data.results)
+            })
+            .catch(erro => {
+                reject(erro)
+            })
+    });
+}
+export const offAirConditioning = () => {
+    return new Promise((resolve, reject) => {
+        axios.get(`${api}/films/`)
+            .then(dados => {
+                resolve(dados.data.results)
+            })
+            .catch(erro => {
+                reject(erro)
+            })
+    });
+}
+export const onAirConditioning = () => {
+    return new Promise((resolve, reject) => {
+        axios.get(`${api}`)
+            .then(dados => {
+                resolve(dados.data.results)
+            })
+            .catch(erro => {
+                reject(erro)
+            })
+    });
+}
+export const getSpecies = () => {
+    return new Promise((resolve, reject) => {
+        axios.get(`${api}/species/`)
+            .then(dados => {
+                resolve(dados.data.results)
+            })
+            .catch(erro => {
+                reject(erro)
+            })
+    });
+}
+
+export const getStarships = () => {
+    return new Promise((resolve, reject) => {
+        axios.get(`${api}/starships/`)
+            .then(dados => {
+                resolve(dados.data.results)
+            })
+            .catch(erro => {
+                reject(erro)
+            })
+    });
+}
