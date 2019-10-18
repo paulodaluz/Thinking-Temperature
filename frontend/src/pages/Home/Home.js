74f62f281e342c6032786e9c200e9f4407e14cf4
Paulo da Luz <paulo.luz@compasso.com.br>
2019-10-18 16:06:24 -0300

    Criação da Home

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -6,14 +6,45 @@ import './Home.css';
 // Import Componentes
 
 
-export default class Contronlador extends Component {
+export default class Home extends Component {
+    constructor(){
+        super()
+        this.state = {
+            pessoas: 17
+        }
+    }
+
+    automaticModeAirConditioning(){
+        console.log('Modo Automático ativado')
+    }
+
+    offAirConditioning(){
+        console.log('Modo Automático ativado')
+    }
+
+    onAirConditioning() {
+        console.log('Modo Automático ativado')
+    }
+
     render() {
         return (
-            <div className="imagem-background-home">
+            <div className="imagem-background-home padding-principal">
+                <div className="painel">
+                    <h3 className="title-inside-table">Temperatura</h3>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <h3 className="title-inside-table">Quantidade de pessoas dentro da sala</h3>
+                    <h1 className="numero-de-pessoas">{this.state.pessoas}</h1>
+                </div>
                 <div className="div-botoes">
-                    <button className="botao">Ativar Modo Automático</button>
-                    <button className="botao">Desligar Ar Condicionado</button>
-                    <button className="botao">Ligar Ar Condicionado</button>
+                    <button className="botao" onClick={() => this.automaticModeAirConditioning()}>Ativar Modo Automático</button>
+                    <button className="botao" onClick={() => this.offAirConditioning()}>Desligar Ar Condicionado</button>
+                    <button className="botao" onClick={() => this.onAirConditioning()}>Ligar Ar Condicionado</button>
                 </div>
             </div>
         )
