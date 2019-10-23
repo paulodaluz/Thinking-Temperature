0798f5ea9dfdbcf4f04ff40bf26da8c9785a921b
Paulo da Luz <paulo.luz@compasso.com.br>
2019-10-23 16:29:55 -0300

    Conectando ao firebase

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 
 // Importando CSS
 import './Home.css';
@@ -11,9 +12,25 @@ export default class Home extends Component {
         super()
         this.state = {
             pessoas: 17,
+            temperatura: [],
             automaticModeAir: false,
-            airConditioning: false,
+            offAirConditioning: false,
+            onAirConditioning: false,
         }
+
+        var firebaseConfig = {
+            apiKey: "AIzaSyBaTlxXD-D6fuovMMfFfEmYVvi5ECG3tFs",
+            authDomain: "thinking-temperature.firebaseapp.com",
+            databaseURL: "https://thinking-temperature.firebaseio.com",
+            projectId: "thinking-temperature",
+            storageBucket: "thinking-temperature.appspot.com",
+            messagingSenderId: "793103532455",
+            appId: "1:793103532455:web:2962e632b25816e865f682",
+            measurementId: "G-N4GLTXMTK5"
+          };
+          // Initialize Firebase
+          firebase.initializeApp(firebaseConfig);
+          this.pegarDados();
     }
 
     automaticModeAirConditioning(){
@@ -21,12 +38,39 @@ export default class Home extends Component {
     }
 
     offAirConditioning(){
+        this.setState.automaticModeAir = false;
+        this.setState.onAirConditioning = false;
+        this.setState.offAirConditioning = true;
         console.log('Modo Automático ativado')
     }
 
     onAirConditioning() {
+        this.setState.automaticModeAir = false;
+        this.setState.offAirConditioning = false;
+        this.setState.onAirConditioning = true;
+        this.salvarDados();
         console.log('Modo Automático ativado')
     }
+
+/*     pegarDados() {
+
+        firebase
+          .database()
+          .ref('/pessoas')
+          .on('value', snapchot => {
+            let dados = snapchot.val()
+            this.setState({ pessoas: 0 })
+            if (dados) {
+              const Keys = Object.keys(dados)
+                console.log(Keys)
+              const pessoas = Keys.map(id => {
+                return { ...dados[id], id }
+              })
+              this.setState({ pessoas: pessoas })
+            }
+        })
+    } */
+      
 
     render() {
         return (
