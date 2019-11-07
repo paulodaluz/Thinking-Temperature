f85379a83e5822dee05993e8004772a1a60c4e58
Paulo da Luz <paulo.luz@compasso.com.br>
2019-11-07 13:40:05 -0300

    Implementação do loading

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -29,7 +29,7 @@ export default class Login extends Component {
     logar = async() => {
         this.setState({ loading: true })
         await login(this.state.email, this.state.password)
-            .then(() => this.props.history.push("/home"))
+            .then(() => this.props.history.push("/home"), this.setState({ loading: false }))
             .catch(erro => this.setState({ errorMessage: erro.message }), this.setState({ loading: false }))
     }
 
