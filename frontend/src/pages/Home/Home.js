242b55a522ea8917c68ffd092d4a23fb4aebbe7e
Paulo da Luz <paulo.luz@compasso.com.br>
2019-11-07 11:16:37 -0300

    Renomeando variaveis

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -11,7 +11,7 @@ export default class Home extends Component {
     constructor(props){
         super(props)
         this.state = {
-            pessoas: 0,
+            qntPessoas: 0,
             temperatura: 0,
             automaticModeAir: false,
             offAirConditioning: false,
@@ -22,11 +22,11 @@ export default class Home extends Component {
 
     atualizarDados() {
         getQntPeople()
-            .then(dados => this.setState({ pessoas: dados }))
+            .then(qntPessoas => this.setState({ qntPessoas }))
             .catch(erro => console.log(erro))
 
         getTemperature()
-            .then(teste => this.setState({ temperatura: teste }))
+            .then(temperatura => this.setState({ temperatura }))
             .catch(erro => console.log(erro))
     }
 
@@ -58,7 +58,7 @@ export default class Home extends Component {
                     <h3 className="title-inside-table">
                         Quantidade de pessoas dentro da sala
                         </h3>
-                    <h1 className="numero-de-pessoas">{this.state.pessoas}</h1>
+                    <h1 className="numero-de-pessoas">{this.state.qntPessoas}</h1>
                 </div>
                 <div className="div-botoes">
                     
