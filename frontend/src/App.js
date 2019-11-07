dbab1b4134d7a412411c48245faa3b75631677ae
Paulo da Luz <paulo.luz@compasso.com.br>
2019-11-07 14:12:10 -0300

    Ajustes de sessão

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, Redirect } from 'react-router-dom';
+import { isAuthenticated } from './services/auth';
 
 // Import CSS
 import './App.css';
@@ -9,12 +10,24 @@ import Login from './pages/Login/Login';
 import Home from './pages/Home/Home';
 
 function App() {
+
+  const PrivateRoute = ({ component: Component }) => (
+    <Route
+      render={props => isAuthenticated() === true ? (
+        <Component {...props} />
+      ) : (
+          <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+        )}
+    />
+  )
+
   return (
     <div>
       <HashRouter>
         <Switch>
           <Route path="/" exact={true} component={Login} />
-          <Route path="/home" exact={true} component={Home} />
+          <PrivateRoute path="/home" exact={true} component={Home} />
+          <Route path='*' component={Login} />
         </Switch>
       </HashRouter>
     </div>
